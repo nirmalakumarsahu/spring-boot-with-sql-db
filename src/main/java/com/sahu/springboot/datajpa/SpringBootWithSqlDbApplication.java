cdb14b71b0070491d1a87d4c9ea7711fc304bbb7
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-09-07 17:49:49 +0530

    Initial Commit

---
+++ src/main/java/com/sahu/springboot/datajpa/SpringBootWithSqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSqlDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sahu/springboot/datajpa/SpringBootWithSqlDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSqlDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
