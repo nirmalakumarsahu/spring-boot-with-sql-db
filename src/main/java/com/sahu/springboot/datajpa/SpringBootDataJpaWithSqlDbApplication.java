1e9cb879ac2c71bae68d5c5278eaeab0619e66d8
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-09-08 21:37:32 +0530

    add the proper name

--- src/main/java/com/sahu/springboot/datajpa/SpringBootDataJpaWithSqlDbApplication.java
+++ src/main/java/com/sahu/springboot/datajpa/SpringBootDataJpaWithSqlDbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWithSqlDbApplication {
+public class SpringBootDataJpaWithSqlDbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWithSqlDbApplication.class, args);
+		SpringApplication.run(SpringBootDataJpaWithSqlDbApplication.class, args);
 	}
 
 }
